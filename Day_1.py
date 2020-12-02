e482babf4787211934d5b078e7f954c3926f2c1d
roelzaps <roel.zaportiza@gmail.com>
2020-12-02 12:57:21 +0800

    this is new solution

--- Day_1.py
+++ Day_1.py
@@ -1,12 +1,21 @@
-#  find to entries that sum to 2020 and then multiply those number together
+# Part 1:  find to entries that sum to 2020 and then multiply those number together
+# Part 2: find the product of three entries that sum to 2020
 
 import itertools
+import numpy as np
 
 with open('data.txt','r') as file:
     data = list(map(int,file.read().split()))
 
-comb = itertools.combinations(data, 2)
+def productcomb(itemlist, numcomb):
+    comb = itertools.combinations(itemlist, numcomb)
+    for i in comb:
+        if sum(list(i)) == 2020:
+            print(np.prod(i))
 
-for i in comb:
-    if sum(list(i)) == 2020:
-        print(i[0]*i[1])
+
+# Part 1 solution
+productcomb(data, 2)
+
+# Part 2 solution
+productcomb(data,3)
